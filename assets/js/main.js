9b7a43d0239dd0447de7ee9c67f3ac262ab10422
alvan nwanorim <alvannwanorim@gmail.com>
2021-11-12 16:08:44 +0100

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,73 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 //   });
 // }
 // window.addEventListener("scroll", scrollactive);
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+// =======================SHOW SCROLL TOP===================
+
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+
+  if (this.scrollY >= 560) {
+    scrollTop.classList.add("show-scroll");
+  } else {
+    scrollTop.classList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollTop);
+
+// ============================DARK LIGHT THEME===================
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () => {
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+};
+const getCurrentIcon = () => {
+  document.body.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+};
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate or deactivate the theme manually with the button
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
